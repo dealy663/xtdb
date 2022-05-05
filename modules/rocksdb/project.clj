325c8c26ba8b5b200bef7f276151600a3657eb6a
Derek <dealy663@gmail.com>
2022-05-04 17:49:12 -0700

    Changed persistance DB from LMBD to RocksDB. LMDB doesn't handle multi-threaded access very well apparently. There are still a few issues with getting all of the onsite blocks updated after cataloging

--- modules/rocksdb/project.clj
+++ modules/rocksdb/project.clj
@@ -15,4 +15,5 @@
                  [com.xtdb/xtdb-core]
                  [com.xtdb/xtdb-metrics :scope "provided"]
                  [org.rocksdb/rocksdbjni "6.12.7"]
+                 ;[org.rocksdb/rocksdbjni "6.29.4.1"]
                  [com.github.jnr/jnr-ffi "2.1.12"]])
